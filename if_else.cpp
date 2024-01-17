6ebd1007c110f171b5b57bd37eea18bdbb10119a
sakshi969 <126317678+sakshi969@users.noreply.github.com>
2024-01-17 10:42:09 +0530

    Add files via upload

---
+++ if_else.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x;
+    cout<< "Enter a number:";
+    cin>> x;
+    if(x>=18)
+    {cout<<"you are an adult ";
+    }
+    else
+    {
+        cout<<"you are not an adult";
+    }
+
+    return 0;
+}
