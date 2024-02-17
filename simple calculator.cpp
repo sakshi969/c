997ff76083ce1264472b179acbbac1ad84ba2210
sakshi969 <126317678+sakshi969@users.noreply.github.com>
2024-02-18 01:01:35 +0530

    Add files via upload

---
+++ simple calculator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a, b;
+    int sum;
+    int diff;
+    
+    cin>>a;
+    cin>>b;
+    sum=a+b;
+    diff=a-b;
+    
+    cout << "Sum is: " << sum << endl;
+    cout << "Difference is: "<< diff << endl;
+
+}
