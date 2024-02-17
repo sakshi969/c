fdcdb7a95fd5ca4544eee12cf6aca49f66eed937
sakshi969 <126317678+sakshi969@users.noreply.github.com>
2024-02-18 01:30:46 +0530

    Add files via upload

---
+++ array indexing.cpp
@@ -0,0 +1,17 @@
+//Write a program which does the following
+
+//The 3rd month in the given list is incorrect
+//Update the 3rd month in the given array with the correct one - "Mar"
+//Once the 3rd array element is updated, output the 3rd array element to the console
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  string mnts[4] = {"Jan", "Feb", " Dec", "Apr"};
+  mnts[2 ] = "Mar";
+  cout << mnts[2];
+
+  
+}
